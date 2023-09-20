c062ddfe4c65a564700d2cb601f869fde2e3282d
GRUDE <kristoffenbg@gmail.com>
2023-09-20 16:32:10 +0200

    create simple graph visualisation of temprature and consumption

--- visualization.py
+++ visualization.py
@@ -4,9 +4,24 @@
 
 # %%
 from preprocessing import read_consumption_data
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
 
 df = read_consumption_data()
 
-print(df.head())
+# Graph of Temperature development
+plt.xlabel('Time')
+plt.ylabel('Temprature')
+plt.title('Temperature for each day')
+plt.plot(df["time"], df["temperature"], 'r')
+
+
+# Graph of Energy Consumption development
+plt.xlabel('Time')
+plt.ylabel('Consumption')
+plt.title('Energy Consumption per day')
+plt.plot(df["time"], df["consumption"], 'r')
+
 
 # %%
