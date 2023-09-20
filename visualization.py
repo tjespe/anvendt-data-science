736359f3df076e18c079a7bd81782c060cc71751
Tord Johan Espe <tordjohanespe@gmail.com>
2023-09-20 16:46:44 +0200

    Add Histogram for Temperature

--- visualization.py
+++ visualization.py
@@ -13,8 +13,9 @@ df = read_consumption_data()
 # %%
 # Graph of Temperature development
 
-daily_avg_temp = df.groupby([df["time"].dt.date, "location"])[
-    "temperature"].mean().unstack()
+daily_avg_temp = (
+    df.groupby([df["time"].dt.date, "location"])["temperature"].mean().unstack()
+)
 # Loop through each city and plot its daily average temperature
 for city in daily_avg_temp.columns:
     plt.plot(daily_avg_temp.index, daily_avg_temp[city], label=city)
@@ -32,11 +33,19 @@ plt.tight_layout()
 plt.show()
 
 # %%
+# Histogram of all temperature values in dataset
+plt.hist(df["temperature"], bins=100)
+plt.xlabel("Temperature")
+plt.ylabel("Frequency")
+plt.title("Histogram of Temperature")
+plt.show()
+
+# %%
 # Graph of Energy Consumption development
-plt.xlabel('Time')
-plt.ylabel('Consumption')
-plt.title('Energy Consumption per day')
-plt.plot(df["time"], df["consumption"], 'r')
+plt.xlabel("Time")
+plt.ylabel("Consumption")
+plt.title("Energy Consumption per day")
+plt.plot(df["time"], df["consumption"], "r")
 
 
 # %%
